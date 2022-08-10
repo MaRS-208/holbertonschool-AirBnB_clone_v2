d3761465c2a9ae4276dfbe9b8b75d8dfa337546d
labalabadie <labalabadie@gmail.com>
2022-08-09 18:08:09 -0700

    trying checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        for k, v in self.__objects:
+            if k.split('.') [0] == cls:
+                new_list.append(k, v)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +28,16 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects"""
+        if not obj:
+            return
+        for k, v in self.__objects.items():
+            if obj == v:
+                del v
+
+
 
     def reload(self):
         """Loads storage dictionary from file"""
