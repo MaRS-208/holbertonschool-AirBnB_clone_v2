6b5254104ff3358cacddba8463c2a971613ab6bf
labalabadie <labalabadie@gmail.com>
2022-08-10 15:53:04 -0700

    deleted caca

--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String
-
-class State(BaseModel):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
