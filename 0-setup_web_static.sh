ba6cc3798260c8b207c31e3d69a839c25709d06e
MaRS-208 <manuela.riossosa@gmail.com>
2022-08-25 23:13:20 -0300

    0. Prepare your web serversIII

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,19 +3,17 @@
 
 sudo apt -y update
 sudo apt -y install nginx
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "
-<html>
+sudo mkdir -p /data/web_static/shared/
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "50i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i "23i location /hbnb_static/ {\nalias /data/web_static/current/;\n}" /etc/nginx/sites-available/default
 sudo service nginx restart
